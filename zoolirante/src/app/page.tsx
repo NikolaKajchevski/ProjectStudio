0fb8cdb6659fca04b83a096cef5eecf8ef13c74f
siangceuthang <ramsy049@mymail.unisa.edu.au>
2025-09-09 12:06:17 +0930

    grid testing on homepage

--- zoolirante/src/app/page.tsx
+++ zoolirante/src/app/page.tsx
@@ -8,6 +8,17 @@ export default function Home() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <h1 className="text-3xl font-light text-gray-500">Zoolirante Homepage</h1>
       </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="flex items-center space-x-3">
+        <Image
+          src="/zoolirante-logo.png"
+          alt="Zoolirante Logo"
+          width={60}
+          height={20}
+          priority
+        />
+       </div>
+      </div>
     </div>
   );
 }
