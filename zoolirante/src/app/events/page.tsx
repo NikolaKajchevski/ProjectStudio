96c119049a4ae1333c144afd9ef043dfaaab3915
Latte-HM <lildrdragon@gmail.com>
2025-09-09 11:50:25 +0930

    test

--- zoolirante/src/app/events/page.tsx
+++ zoolirante/src/app/events/page.tsx
@@ -5,18 +5,18 @@ export default function EventsPage() {
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
     const formData = new FormData(event.currentTarget)
-    const response = await fetch('/api/events', {
+    const response = await fetch('api/route', {
       method: 'POST',
       body: formData,
     })
-    'use client';
     // Handle response if necessary
     const data = await response.json()
     // ...
   }
   return (
     <form onSubmit={onSubmit}>
-      <input type="text" name="name" />
+      <input type="text" name="first" />
+      <input type="text" name="second"/>
       <button type="submit">Submit</button>
     </form>
   );
