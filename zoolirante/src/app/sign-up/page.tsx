736992874de50173665371deede8b81625fe8ea1
mahtj002 <mahtj002@mymail.unisa.edu.au>
2025-09-14 10:34:30 +0930

    Updated signup page form details

--- zoolirante/src/app/sign-up/page.tsx
+++ zoolirante/src/app/sign-up/page.tsx
@@ -4,90 +4,74 @@ export default function SignUpPage() {
     <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
     
         <div className="md:col-span-5 space-y-4 p-4 bg-white border border-gray-300 rounded-lg w-full">
-            <h2 className="text-xl font-bold text-orange-500 mb-4">Sign Up</h2>
-
-            <div>
-                <label className="block text-sm font-medium text-gray-700 mb-1">Select Date</label>
-                <input
-                type="date"
-                max={new Date().toISOString().split("T")[0]} // Cannot select a future date
-                className="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:border-orange-500 focus:ring-2 focus:ring-orange-300"
-                />
-            </div>
+            <h2 className="text-xl font-bold text-orange-500 mb-1">Sign Up</h2>
+            <p className="text-sm mb-4">Already have an account? 
+                <a href="/login" className="text-blue-500"> Login </a></p>
 
             <div className="flex space-x-2">
                 <div className="flex-1">
-                    <label className="block text-sm font-medium text-gray-700 mb-1">First Name</label>
+                    <label className="block text-sm font-medium text-gray-700 mb-1">
+                        First Name <span className="text-red-500">*</span> </label>
                     <input
                     type="text"
                     placeholder="First Name"
+                    required
                     className="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-orange-500"
                     />
                 </div>
 
                 <div className="flex-1">
-                    <label className="block text-sm font-medium text-gray-700 mb-1">Last Name</label>
+                    <label className="block text-sm font-medium text-gray-700 mb-1">
+                        Last Name <span className="text-red-500">*</span> </label>
                     <input
                     type="text"
                     placeholder="Last Name"
+                    required
                     className="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-orange-500"
                     />
                 </div>
             </div>
 
             <div>
-                <label className="block text-sm font-medium text-gray-700 mb-1">Email</label>
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                    Email <span className="text-red-500">*</span> </label>
                 <input
                 type="email"
+                required
                 placeholder="janedoe@example.com"
-                className="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:border-orange-500 focus:ring-2 focus:ring-orange-300"
-            />
-        </div>
-
-        <div className="flex space-x-2">
-            
-            {/* Password */}
-            <div className="flex-1">
-                <label className="block text-sm font-medium text-gray-700 mb-1">Password</label>
-                <input
-                type="password"
-                placeholder="MyStrongPass123"
-                maxLength={19}
-                
                 className="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-orange-500"
                 />
             </div>
 
-            {/* CVC */}
-            <div className="w-20">
-                <label className="block text-sm font-medium text-gray-700 mb-1">CVC</label>
+            <div>
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                    Password <span className="text-red-500">*</span> </label>
                 <input
-                type="text"
-                placeholder="123"
-                maxLength={3}
-                inputMode="numeric"
-                className="w-full border border-gray-300 rounded-lg p-2 text-center focus:outline-none focus:ring-2 focus:ring-orange-500"
+                type="password"
+                required
+                placeholder="MyStrongPass123"
+                maxLength={19}
+                    
+                className="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-orange-500"
                 />
             </div>
-
-            {/* Expiry */}
-            <div className="w-24">
-                <label className="block text-sm font-medium text-gray-700 mb-1">Expiry</label>
+            
+            <div className="flex items-center space-x-2">
                 <input
-                type="text"
-                placeholder="MM/YY"
-                maxLength={5}
-                className="w-full border border-gray-300 rounded-lg p-2 text-center focus:outline-none focus:ring-2 focus:ring-orange-500"
+                    type="checkbox"    
+                    className="h-4 w-4 text-orange-500 border-gray-300 rounded focus:ring-orange-500"
                 />
-            </div>
-            </div>
 
-        <button className="bg-orange-500 text-white px-4 py-2 rounded-full text-sm hover:bg-orange-600 transition-colors w-full">
-            Proceed
-        </button>
-      </div>
+                <label className="text-sm text-gray-700">
+                    Sign me up for the mailing list </label>
+            </div>
+            
+            <button className="bg-orange-500 text-white px-4 py-2 rounded-full text-sm hover:bg-orange-600 transition-colors w-full">
+                Sign Up
+            </button>
+        </div>
     </div>
-  </div>
+    </div>
 )};
 
 
