be8663293f2c19728522e980ac22229a8f5b7660
nguky079 <nguky079@mymail.unisa.edu.au>
2025-09-15 22:43:52 +0930

    Added images and formatted shop page

--- zoolirante/src/app/shop/page.tsx
+++ zoolirante/src/app/shop/page.tsx
@@ -18,10 +18,10 @@ export default async function Shop() {
         <div className= 'grid grid-cols-3 gap-x-2 gap-y-3'>
         {zooData.merchandise.map((merch: any) => (
 
-
+          
           <Link 
            href={`/shop/${merch.id}`}
-          className="bg-white rounded-lg shadow-xl min-h-[50px] overflow-hidden">
+          className="bg-white  mt-10  rounded-lg shadow-xl min-h-[50px] overflow-hidden">
 
 
             <div className="relative h-48">
@@ -33,11 +33,11 @@ export default async function Shop() {
                 style={{ objectFit: 'cover' }}
               />
             </div >
-                <h2 className="text-black font-large" > <b> {merch.name}</b> </h2>
-                <p className="text-gray-600 font-medium" >  {merch.category} </p>
-                <p>Description: </p>
-                <p className="text-gray-600 font-medium" >  {merch.description} </p>
-                <p className="text-gray-600 font-medium" >  ${merch.price} </p>
+                <h2 className="ml-5 mt-10 text-black font-large" > <b> {merch.name}</b> </h2>
+                <p className="ml-5 text-orange-500 font-medium" >  {merch.category} </p>
+                <p className="ml-5 mt-10  text-black font-medium">Description: </p>
+                <p className="ml-5 text-black font-medium" >  {merch.description} </p>
+                <p className="ml-5 text-green-900 mb-5 font-medium" >  ${merch.price} </p>
 
           </Link>
 
